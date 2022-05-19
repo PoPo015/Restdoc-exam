82dd68b486101f70a46b06e4382df241a68d0e45
PoPo015 <kst005109@naver.com>
2022-05-19 18:07:39 +0900

    feat : service layer test 코드 (연습) 작성중

--- src/test/java/com/example/restdocexam/testcode/service/OrderSerivceTest.java
+++ src/test/java/com/example/restdocexam/testcode/service/OrderSerivceTest.java
@@ -1,16 +1,23 @@
 package com.example.restdocexam.testcode.service;
 
 import com.example.restdocexam.testcode.domain.dto.OrderRequest;
+import com.example.restdocexam.testcode.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class OrderSerivceTest {
 
-    @Autowired
-    private OrderSerivce orderSerivce;
+    @Mock
+    private OrderRepository orderRepository;
 
+    @InjectMocks
+    private OrderSerivce orderSerivce;
 
     @Test
     public void given() throws Exception{
